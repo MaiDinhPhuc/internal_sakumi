f95a754e0fe7f85d705d02b5c35ce5f2e9445b2d
Minh Nguyễn <88758271+NthienMinh@users.noreply.github.com>
2023-09-06 10:25:00 +0700

    upload data

--- lib/configs/app_configs.dart
+++ lib/configs/app_configs.dart
@@ -2,7 +2,7 @@ class AppConfigs{
 
   static const String endpoint = "http://173.199.127.90:3000";
 
-  static const bool endpointFirebaseDebug = true;
+  static const bool endpointFirebaseDebug = false;
 
   static String dir = "";
   static String defaultImage = 'https://cdn3.iconfinder.com/data/icons/education-1-28/49/144-512.png';
