6413a95ba157a95345c5c64b2b453b37926acd27
Minh Nguyễn <88758271+NthienMinh@users.noreply.github.com>
2023-09-08 15:00:12 +0700

    update link

--- lib/configs/app_configs.dart
+++ lib/configs/app_configs.dart
@@ -5,7 +5,8 @@ class AppConfigs{
   static const bool endpointFirebaseDebug = false;
 
   static String getDataUrl(String file, String token){
-    return "$endPoint/api/v1/mobile/static/get/?token=$token&name=$file";
+    //e6cceec3aec49617978e81ec107d6b618738f757547c602a8fdd2af2cfd5836d721ba6c2943e38f5914adb2eaccdfb9cf3ce4f97fe50ce5f81d6fa4b20a45f14/btvn_102501.json
+    return "https://noibo.sakumi.edu.vn/files/$token/$file";
   }
 
   static String dir = "";
