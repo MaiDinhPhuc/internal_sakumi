1069f5ac07fa4cbda23257f813b34a447d172eea
maiphuc <maidinhphucpk@gmail.com>
2023-11-27 20:11:02 +0700

    edit record

--- lib/features/admin/tool/alert_input_field.dart
+++ lib/features/admin/tool/alert_input_field.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:internal_sakumi/configs/text_configs.dart';
 import 'package:internal_sakumi/utils/resizable.dart';
@@ -49,7 +48,6 @@ class InputField extends StatelessWidget {
     );
   }
 }
-
 void alertInputField(BuildContext context, TextEditingController controller,
     Function()? onPressed) {
 
@@ -61,10 +59,12 @@ void alertInputField(BuildContext context, TextEditingController controller,
             insetPadding: EdgeInsets.all(Resizable.padding(context, 15)),
             child: Container(
               padding: EdgeInsets.all(Resizable.padding(context, 10)),
+              width: MediaQuery.of(context).size.width*2/3,
               child: SingleChildScrollView(
                 child: Column(
                   children: [
                     InputField(controller: controller),
+                    SizedBox(height: Resizable.size(context, 10)),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: [
@@ -77,6 +77,67 @@ void alertInputField(BuildContext context, TextEditingController controller,
                               controller.text = '';
                             },
                             child: Text(AppText.btnRemove.text)),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ));
+      });
+}
+
+void alertEditField(BuildContext context, String text,
+    {required Function() onPressed, required Function(String) onChanged}) {
+  showDialog(
+      context: context,
+      builder: (_) {
+        return Dialog(
+            backgroundColor: Colors.white,
+            insetPadding: EdgeInsets.all(Resizable.padding(context, 15)),
+            child: Container(
+              padding: EdgeInsets.all(Resizable.padding(context, 10)),
+              width: MediaQuery.of(context).size.width/2,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      style: TextStyle(
+                          fontSize: Resizable.font(context, 18), fontWeight: FontWeight.w500),
+                      autofocus: true,
+                      initialValue: text,
+                      decoration: InputDecoration(
+                        isDense: true,
+                        fillColor: Colors.white,
+                        hoverColor: Colors.transparent,
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: const Color(0xffE0E0E0),
+                              width: Resizable.size(context, 0.5)),
+                          borderRadius: BorderRadius.circular(Resizable.padding(context, 5)),
+                        ),
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(Resizable.padding(context, 5)),
+                            borderSide: BorderSide(
+                                color: const Color(0xffE0E0E0),
+                                width: Resizable.size(context, 0.5))),
+                      ),
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 1,
+                      onChanged: onChanged,
+                    ),
+                    SizedBox(height: Resizable.size(context, 10)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        ElevatedButton(
+                            onPressed: onPressed, child: Text(AppText.btnEdit.text)),
+                        SizedBox(width: Resizable.size(context, 10)),
+                        ElevatedButton(
+                            onPressed: (){
+                              Navigator.pop(context);
+                            },
+                            child: Text(AppText.txtExit.text)),
                       ],
                     )
                   ],
--- lib/screens/admin/tab/tool_screen.dart
+++ lib/screens/admin/tab/tool_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/Material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:internal_sakumi/configs/text_configs.dart';
 import 'package:internal_sakumi/features/admin/app_bar/admin_appbar.dart';
 import 'package:internal_sakumi/features/admin/tool/google_sign_in.dart';
 
@@ -9,10 +7,10 @@ class ToolScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Column(
         children: [
-          const AdminAppBar(index: 5),
+          AdminAppBar(index: 5),
           Expanded(child: SignInDemo())
         ],
       ),
