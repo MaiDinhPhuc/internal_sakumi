34ef7af713c6da1fc4c05d9500be4e78b2d3b48c
thaibinhduong <duongtb1992@gmail.com>
2023-12-28 16:18:15 +0700

    cache coyrse

--- lib/providers/cache/cached_data_provider.dart
+++ lib/providers/cache/cached_data_provider.dart
@@ -6,50 +6,69 @@ import 'package:internal_sakumi/providers/firebase/firebase_provider.dart';
 
 import 'filter_admin_provider.dart';
 
-
 class CacheObject {
   Object? data;
-  DateTime? time;
+  DateTime time;
 
-  List<Function()> callbacks = [];
+  CacheObject(this.time, {this.callbacks = const []});
+
+  List<Function(Object)> callbacks;
 }
 
 class DataProvider {
   static Map<String, CacheObject> cached = {};
 
-  static Future<List<ClassModel>> clasList(Map<AdminFilter, List> filter, int admin, {ClassModel? lastItem}) async {
-
+  static Future<List<ClassModel>> clasList(
+      Map<AdminFilter, List> filter, int admin,
+      {ClassModel? lastItem}) async {
     List? listType = filter[AdminFilter.type];
     List? listStatus = filter[AdminFilter.status];
     List<int> listTypeQuery = listType!.map((e) => type(e)).toList();
     List<String> listStatusQuery = listStatus!.map((e) => status(e)).toList();
-    var listClass = await FireBaseProvider.instance.getClassWithFilter(listStatusQuery,listTypeQuery);
+    var listClass = await FireBaseProvider.instance
+        .getClassWithFilter(listStatusQuery, listTypeQuery);
     return listClass;
   }
 
   static String status(FilterClassStatus status) {
-    switch(status) {
-      case FilterClassStatus.preparing : return  "Preparing";
-      case FilterClassStatus.completed : return  "Completed";
-      case FilterClassStatus.studying : return  "InProgress";
-      case FilterClassStatus.cancel : return  "Cancel";
+    switch (status) {
+      case FilterClassStatus.preparing:
+        return "Preparing";
+      case FilterClassStatus.completed:
+        return "Completed";
+      case FilterClassStatus.studying:
+        return "InProgress";
+      case FilterClassStatus.cancel:
+        return "Cancel";
     }
   }
-
 
   static int type(FilterClassType type) {
-    switch(type) {
-      case FilterClassType.group : return 0;
-      case FilterClassType.one : return 1;
+    switch (type) {
+      case FilterClassType.group:
+        return 0;
+      case FilterClassType.one:
+        return 1;
     }
   }
 
-  static Future<CourseModel> courseById(int id) async {
-    if(cached['course_$id'] == null){
-      return await FireBaseProvider.instance.getCourseById(id);
+  static Future<void> courseById(int id, Function(Object) onLoaded) async {
+
+    if (cached['course_$id'] == null) {
+      cached['course_$id'] = CacheObject(DateTime.now(), callbacks: [onLoaded]);
+      cached['course_$id']!.data =
+          await FireBaseProvider.instance.getCourseById(id);
+
+      for (var element in cached['course_$id']!.callbacks) {
+        element.call(cached['course_$id']!.data!);
+      }
+      cached['course_$id']!.callbacks = [];
+    } else if (cached['course_$id']!.data == null) {
+      cached['course_$id']!.callbacks.add(onLoaded);
+    } else {
+      onLoaded.call(cached['course_$id']!.data!);
     }
-    return;
+
+
   }
-
-
 }
