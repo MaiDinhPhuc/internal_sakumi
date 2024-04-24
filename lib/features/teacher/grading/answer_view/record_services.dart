b8471007fee5d457c691edda9942dd27be1d7ec4
NthienMinh <88758271+NthienMinh@users.noreply.github.com>
2024-04-24 09:43:22 +0700

    fix record

--- lib/features/teacher/grading/answer_view/record_services.dart
+++ lib/features/teacher/grading/answer_view/record_services.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:microphone/microphone.dart';
 import 'package:http/http.dart' as http;
-import 'package:record/record.dart';
+// import 'package:record/record.dart';
 class RecordService {
   RecordService._privateConstructor();
 
@@ -9,12 +9,15 @@ class RecordService {
 
   static RecordService get instance => _instance;
 
-  AudioRecorder? _recorder;
+  MicrophoneRecorder? _recorder;
+  //AudioRecorder? _recorder;
 
-  Future<AudioRecorder> newRecorder()  async {
+  //Future<AudioRecorder> newRecorder()  async {
+  Future<MicrophoneRecorder> newRecorder()  async {
+   // _recorder = AudioRecorder();
+    _recorder = MicrophoneRecorder();
 
-    _recorder = AudioRecorder();
-
+    await _recorder!.init();
     return _recorder!;
   }
 
@@ -25,27 +28,29 @@ class RecordService {
   }
 
   start()async{
-    var record = await newRecorder();
-    if (await record.hasPermission()) {
-      // Start recording to file
-      await record.start(const RecordConfig(), path: '');
-    }
+    var recorder = await newRecorder();
+    await recorder.start();
+    //var record = await newRecorder();
+    // if (await record.hasPermission()) {
+    //   // Start recording to file
+    //   await record.start(const RecordConfig(), path: '');
+    // }
 
   }
 
   Future<String> stop()async {
-     await _recorder!.stop();
-     return '';
-// _recorder!.
-//      final recordingUrl = _recorder!.value.recording!.url;
-//      http.Response response = await http.get(
-//          Uri.parse(recordingUrl)
-//      );
-//      final now = DateTime.now().microsecondsSinceEpoch;
-//      final ref = FirebaseStorage.instance.ref().child('grading_result_record/$now');
-//      await ref.putData(response.bodyBytes, SettableMetadata(contentType: 'audio/mpeg'));
-//      var link = await ref.getDownloadURL();
-//      return link;
+      await _recorder!.stop();
+     // return '';
+//_recorder!.
+     final recordingUrl = _recorder!.value.recording!.url;
+     http.Response response = await http.get(
+         Uri.parse(recordingUrl)
+     );
+     final now = DateTime.now().microsecondsSinceEpoch;
+     final ref = FirebaseStorage.instance.ref().child('grading_result_record/$now');
+     await ref.putData(response.bodyBytes, SettableMetadata(contentType: 'audio/mpeg'));
+     var link = await ref.getDownloadURL();
+     return link;
   }
 
 
