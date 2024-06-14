af6e7d7144b9b1ea8cac597bd2cdd2d4b7a78b39
porostudio <168701728+porostudio@users.noreply.github.com>
2024-06-14 10:48:11 +0700

    update

--- lib/configs/app_configs.dart
+++ lib/configs/app_configs.dart
@@ -13,6 +13,10 @@ class AppConfigs{
     return "https://noibo.sakumi.edu.vn/files/$token/$file";
   }
 
+  static String getDownloadUrl(String fileToken, String token){
+    return 'http://173.199.127.90:3000/api/v1/mobile/static/get/?token=$token&name=$fileToken';
+  }
+
   static String defaultImage = 'https://cdn3.iconfinder.com/data/icons/education-1-28/49/144-512.png';
 
   static const meetRecordingsId = '1IAcWu52mBEBMiOySR6M8IGb1DZYzma50';
